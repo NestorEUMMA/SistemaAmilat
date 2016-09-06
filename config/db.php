bf06119b0fa9a9c57744fa8045986683db5297c2
Nestor Ulloa <ulloa.nestor22@gmail.com>
2016-09-05 19:55:12 -0600

    a

--- config/db.php
+++ config/db.php
@@ -2,10 +2,18 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=db_clinica',
-    'username' => 'root',
+    //'dsn' => 'mysql:host=localhost;dbname=db_clinica',
+	'dsn' => 'mysql:host=MYSQL5009.SmarterASP.NET;dbname=db_9b35fa_clinica',
+    //'username' => 'root',
+	'username' => '9b35fa_clinica',
     //'password' => 'hope2900clinic',
+<<<<<<< HEAD
     'password' => 'nest2013',
     //'password' => '12345',
+=======
+    //'password' => 'nest2013',
+    //'password' => '12345',
+	'password' => 'clinica2016',
+>>>>>>> 2e4fa8ab64c4bf93cec674c56cdaa344bedcb62d
     'charset' => 'utf8',
 ];
--- include/dbconnect.php
+++ include/dbconnect.php
@@ -1,12 +1,24 @@
 <?php
 
+<<<<<<< HEAD
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'nest2013');
 //define('DB_PASSWORD', '12345');
 define('DB_DATABASE', 'db_clinica');
+=======
+//define('DB_SERVER', 'localhost');
+//define('DB_USERNAME', 'root');
+//define('DB_PASSWORD', 'nest2013');
+//define('DB_PASSWORD', '12345');
+//define('DB_DATABASE', 'db_clinica');
+>>>>>>> 2e4fa8ab64c4bf93cec674c56cdaa344bedcb62d
 //define('DB_PASSWORD', 'hope2900clinic');
 //define('DB_PASSWORD', '12345');
+define ('DB_SERVER', 'MYSQL5009.SmarterASP.Net');
+define ('DB_USERNAME', '9b35fa_clinica');
+define ('DB_PASSWORD', 'clinica2016');
+define ('DB_DATABASE', 'db_9b35fa_clinica');
 
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
